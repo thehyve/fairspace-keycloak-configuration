6d0042b8d30d3f67e7d99c05d7609ed03ecf96b2
Robert Horlings <robert@isdat.nl>
2018-10-24 15:34:23 +0200

    Add second test user by default

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Parameters for additional test users. See functions/add-test-users.sh
-USERNAMESPREFIXES=( "user" "user2" "user3" )
+USERNAMESPREFIXES=( "test2" "user" "user2" "user3" )
 COORDINATORPREFIXES=( "coordinator2" "coordinator3" )
-FIRSTNAMES=( "John" "Ygritte" "Daenarys"  "Gregor"  "Cersei"    "Tyrion"    "Arya"  "Sansa" "Khal"  "Joffrey"   "Sandor" )
-LASTNAMES=(  "Snow" ""        "Targaryen" "Clegane" "Lannister" "Lannister" "Stark" "Stark" "Drogo" "Baratheon" "Clegane" )
+FIRSTNAMES=( "Test" "John" "Ygritte" "Daenarys"  "Gregor"  "Cersei"    "Tyrion"    "Arya"  "Sansa" "Khal"  "Joffrey"   "Sandor" )
+LASTNAMES=(  "User" "Snow" ""        "Targaryen" "Clegane" "Lannister" "Lannister" "Stark" "Stark" "Drogo" "Baratheon" "Clegane" )
