4dca28ffb950db619f1e28560a5708297435bf75
Robert Horlings <robert@isdat.nl>
2019-09-06 14:19:28 +0200

    Change keycloak setup to use roles directly

--- scripts/functions/add-policy-for-permission.sh
+++ scripts/functions/add-policy-for-permission.sh
@@ -14,14 +14,19 @@ REALM=$1
 PERMISSION_NAME=$2
 POLICY_NAME=$3
 
+echo "Updating permission $PERMISSION_NAME with policy $POLICY_NAME"
+
 PERMISSION_ID=$(${DIR}/get-permission-id.sh "$REALM" "$PERMISSION_NAME")
-if [ $? -ne 0 ]; then exit 1; fi
+if [ $? -ne 0 ]; then >&2 echo "No permission found for permission $PERMISSION_NAME" && exit 1; fi
 
 POLICY_ID=$(${DIR}/get-policy-id.sh "$REALM" "$POLICY_NAME")
-if [ $? -ne 0 ]; then exit 1; fi
+if [ $? -ne 0 ]; then >&2 echo "No policy found for policy $POLICY_NAME" && exit 1; fi
+
+CURRENT_POLICIES=$(kcadm.sh get clients/$REALM_MANAGEMENT_UUID/authz/resource-server/permission/scope/$PERMISSION_ID/associatedPolicies -r "$REALM" --fields id)
+UPDATED_POLICIES=$(echo $CURRENT_POLICIES | jq "map(.id) + [\"$POLICY_ID\"]" | tr '\n' ' ')
 
 sed \
     -e "s/\${PERMISSION_NAME}/$PERMISSION_NAME/g" \
-    -e "s/\${POLICY_ID}/$POLICY_ID/g" \
+    -e "s/\${POLICIES}/$UPDATED_POLICIES/g" \
     ${DIR}/../workspace-config/update-permission.json | \
     kcadm.sh update clients/$REALM_MANAGEMENT_UUID/authz/resource-server/permission/scope/$PERMISSION_ID -r "$REALM" -f -
--- scripts/functions/get-role-id.sh
+++ scripts/functions/get-role-id.sh
@@ -15,7 +15,7 @@ ROLE_NAME=$2
 ROLE_ID=$(kcadm.sh get roles/$ROLE_NAME -r "$REALM" --fields id --format csv --noquotes)
 
 if [ $? -ne 0 ] || [ -z "$ROLE_ID" ]; then
-    >&2 echo "No role ID could be found for group $ROLE_NAME"
+    >&2 echo "No role ID could be found for role $ROLE_NAME"
     exit 1
 fi
 
