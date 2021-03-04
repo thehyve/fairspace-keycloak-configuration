fc81f623d4d803985fdc0562acaa9448998fe43f
Gijs Kant <gijs@thehyve.nl>
2021-03-04 15:09:36 +0100

    Clean up unused config.

--- scripts/config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Parameters for additional test users. See functions/add-test-users.sh
-export USERNAMES=( "test" "test2" "user" "user2" "datasteward" "datasteward2" "coordinator" "coordinator2" )
-export FIRSTNAMES=( "Test" "John" "Ygritte" "Daenarys"  "Gregor"  "Cersei"    "Tyrion"    "Arya"  "Sansa" "Khal"  "Joffrey"   "Sandor" )
-export LASTNAMES=(  "User" "Snow" ""        "Targaryen" "Clegane" "Lannister" "Lannister" "Stark" "Stark" "Drogo" "Baratheon" "Clegane" )
